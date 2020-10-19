1a30c640378c9d75686e1f7519c76aa2a454c54f
jocarino <joao.goncalo.veloso@gmail.com>
2020-10-19 18:46:31 +0100

    path and fs modules tests

--- person.js
+++ person.js
@@ -7,4 +7,7 @@ class Person {
         console.log(`My name is ${this.name} and I am ${this.age}`)
     }
 }
+
+//this is requiered in node, normal import is yet to be implemented in es6
+//called common node import
 module.exports = Person;
