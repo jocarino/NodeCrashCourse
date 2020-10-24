2386d71fae265c7a90731fc8cb7fd96eaf15b815
Joao Veloso <joao.veloso@auroraer.com>
2020-10-24 16:57:40 +0100

    indext refactor

--- index.js
+++ index.js
@@ -5,24 +5,77 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-    if (request.url === '/') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
-            (err, content) => {
-                if (err) throw err;
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.end(content);
-            })
-    }
-    if (request.url === '/api/users') {
-        const users = [
-            {name:'Bob Smith', age:40},
-            {name:'John Doe',age:30}
-        ];
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(users));
+    /*     if (request.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'public', 'index.html'),
+                (err, content) => {
+                    if (err) throw err;
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.end(content);
+                })
+        }
+        if (request.url === '/api/users') {
+            const users = [
+                {name:'Bob Smith', age:40},
+                {name:'John Doe',age:30}
+            ];
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(users));
+    
+        } */
 
+    // Build file path
+    let filePath = path.join(
+        __dirname,
+        'public',
+        request.url === '/' ? 'index.html' : request.url
+    );
+
+    // Extension of the file
+    let extensionName = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch (extensionName) {
+        case ".js":
+            contentType = "text/javascript";
+            break;
+          case ".css":
+            contentType = "text/css";
+            break;
+          case ".json":
+            contentType = "application/json";
+            break;
+          case ".png":
+            contentType = "image/png";
+            break;
+          case ".jpg":
+            contentType = "image/jpg";
+            break;
     }
+
+    //Read File
+    fs.readFile(filePath,(error, content)=>{
+        if(err){
+            if(err.code=='ENONET'){
+                // Page not found
+                fs.readFile(path.join(__dirname,'public','404.html'),(error, content)=>{
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
+                    response.end(content,'utf8');
+                })
+            } else{
+                // Some server error
+                response.writeHead(500);
+                response.end(`Server Error: ${error.code}`);
+            }
+        }else{
+            // Sucess
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(content,'utf8');
+        }
+    })
 });
 
 // Look for local environment port, otherwise runs port 5000
