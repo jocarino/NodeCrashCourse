0d3fd4ac1f5b90c1e5d41d993df9b59b31b6d04e
jocarino <joao.goncalo.veloso@gmail.com>
2020-10-24 16:08:48 +0100

    index update

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Express.js makes this requests easier to implement
 
-const
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((request, response)=>{
+
+});
+
+// Look for local environment port, otherwise runs port 5000
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
