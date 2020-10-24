3cf0494c6afb7b1263a0ea0863c130653daddadf
Joao Veloso <joao.veloso@auroraer.com>
2020-10-24 16:37:19 +0100

    reading template html

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
-const server = http.createServer((request, response)=>{
-
+const server = http.createServer((request, response) => {
+    if (request.url === '/') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'),
+            (err, content) => {
+                if (err) throw err;
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(content);
+            })
+    }
 });
 
 // Look for local environment port, otherwise runs port 5000
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-//comment
